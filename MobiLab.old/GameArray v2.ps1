d6f17c3944922847bfbee88b1d05363d7ed29ce6
Serg <sergey_osherov@mail.ru>
2021-05-24 13:52:06 +0200

    Delete MobiLab.old directory

--- MobiLab.old/GameArray v2.ps1
+++
@@ -1,7 +0,0 @@
-$arr = ConvertFrom-Csv @'
-Name,   Article,   Size
-David,  TShirt,    M
-Eduard, Trouwsers, S
-'@
-
-$arr[0].Name
--- MobiLab.old/List VM.ps1
+++
@@ -1,3 +0,0 @@
-Select-AzSubscription -SubscriptionID f5b5d7b3-27e6-47ba-83c1-58824d3eb42b
-$rgName = 'rgVmMigrated' # Resource Group Name
-Get-AzVM -ResourceGroupName $rgName | Format-Table -AutoSize
